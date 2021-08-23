6360a158887a23738974f8e003705db1d953470c
Elerson R. S. Santos <elerss@gmail.com>
2021-08-23 13:56:39 -0300

    addomg adaptive loss

--- manydepth/trainer.py
+++ manydepth/trainer.py
@@ -18,6 +18,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from tensorboardX import SummaryWriter
+from robust_loss_pytorch import AdaptiveLossFunction
+
 
 import json
 
