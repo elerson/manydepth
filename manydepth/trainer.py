27d63664930b2afccb9a9413e0427328a6692aeb
Elerson R. S. Santos <elerss@gmail.com>
2021-08-26 20:30:33 -0300

    adding adaptative loss

--- manydepth/trainer.py
+++ manydepth/trainer.py
@@ -633,8 +633,9 @@ class Trainer:
                 consistency_mask = (1 - reprojection_loss_mask).float()
 
             # standard reprojection loss
-            reprojection_loss = reprojection_loss * reprojection_loss_mask
-            reprojection_loss = reprojection_loss.sum() / (reprojection_loss_mask.sum() + 1e-7)
+            #reprojection_loss = reprojection_loss * reprojection_loss_mask
+            #reprojection_loss = reprojection_loss.sum() / (reprojection_loss_mask.sum() + 1e-7)
+            reprojection_loss = reprojection_loss.mean()
 
             # consistency loss:
             # encourage multi frame prediction to be like singe frame where masking is happening
@@ -660,9 +661,9 @@ class Trainer:
 
             mean_disp = disp.mean(2, True).mean(3, True)
             norm_disp = disp / (mean_disp + 1e-7)
-            smooth_loss = get_smooth_loss(norm_disp, color)
+            #smooth_loss = get_smooth_loss(norm_disp, color)
 
-            loss += self.opt.disparity_smoothness * smooth_loss / (2 ** scale)
+            #loss += self.opt.disparity_smoothness * smooth_loss / (2 ** scale)
             total_loss += loss
             losses["loss/{}".format(scale)] = loss
 
