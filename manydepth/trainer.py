37c67d694d87de14ffbaf0b9597b99739dd40629
Elerson R. S. Santos <elerss@gmail.com>
2021-08-27 19:07:36 -0300

    adding adaptative loss

--- manydepth/trainer.py
+++ manydepth/trainer.py
@@ -543,7 +543,7 @@ class Trainer:
             #print('teste')
         else:
             ssim_loss = self.ssim(pred, target).mean(1, True)
-            reprojection_loss = 0.25 * ssim_loss + 0.75 * l1_loss
+            reprojection_loss = 0.5 * ssim_loss + 0.5 * l1_loss
 
         return reprojection_loss
 
