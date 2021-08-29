d333cf1a2d91969676be6411bea0735fcad91c52
Elerson Santos <elerson@gmail.com>
2021-08-29 10:06:44 -0300

    adding the robust skewed loss

--- manydepth/trainer_skewed.py
+++ manydepth/trainer_skewed.py
@@ -121,7 +121,7 @@ class Trainer:
 
 
         image_size = (3, self.opt.height, self.opt.width)
-        self.adaptive_image_loss_func = AdaptiveImageLossFunctionSkewed(image_size, np.float32, 0, alpha_lo=0.001, alpha_hi=1.999, scale_lo=1.0, scale_init=1.0)
+        self.adaptive_image_loss_func = AdaptiveImageLossFunctionSkewed(image_size, np.float32, 0, beta_lo=0.001, beta_hi=1.999, scale_lo=1.0, scale_init=1.0)
 
 
 
